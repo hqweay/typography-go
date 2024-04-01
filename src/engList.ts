9dfb129cfe7a7a7e81a110934c7643fe4b73f645
hqweay <hqweay@gmail.com>
2024-04-02 02:19:29 +0800

    feat#技术名词大小写转换

--- src/engList.ts
+++ src/engList.ts
@@ -2,28 +2,208 @@
 	const engList = [
   
 		{
-      key: /MySQL/gi,
+      key: /\bMySQL\b/gi,
       value: "MySQL",
     }
 		,
 		{
-      key: /Redis/gi,
+      key: /\bRedis\b/gi,
       value: "Redis",
     }
 		,
 		{
-      key: /MacOS/gi,
+      key: /\bMacOS\b/gi,
       value: "MacOS",
     }
 		,
 		{
-      key: /iPhone/gi,
+      key: /\biPhone\b/gi,
       value: "iPhone",
     }
 		,
 		{
-      key: /iPad/gi,
+      key: /\biPad\b/gi,
       value: "iPad",
+    }
+		,
+		{
+      key: /\bJava\b/gi,
+      value: "Java",
+    }
+		,
+		{
+      key: /\bGitHub\b/gi,
+      value: "GitHub",
+    }
+		,
+		{
+      key: /\bGitLab\b/gi,
+      value: "GitLab",
+    }
+		,
+		{
+      key: /\bSpring Boot\b/gi,
+      value: "Spring Boot",
+    }
+		,
+		{
+      key: /\bSpring MVC\b/gi,
+      value: "Spring MVC",
+    }
+		,
+		{
+      key: /\bSpring Cloud\b/gi,
+      value: "Spring Cloud",
+    }
+		,
+		{
+      key: /\bSpring Data JPA\b/gi,
+      value: "Spring Data JPA",
+    }
+		,
+		{
+      key: /\bMyBatis\b/gi,
+      value: "MyBatis",
+    }
+		,
+		{
+      key: /\bHibernate\b/gi,
+      value: "Hibernate",
+    }
+		,
+		{
+      key: /\bOracle\b/gi,
+      value: "Oracle",
+    }
+		,
+		{
+      key: /\bMaven\b/gi,
+      value: "Maven",
+    }
+		,
+		{
+      key: /\bIoC\b/gi,
+      value: "IoC",
+    }
+		,
+		{
+      key: /\bDI\b/gi,
+      value: "DI",
+    }
+		,
+		{
+      key: /\bAOP\b/gi,
+      value: "AOP",
+    }
+		,
+		{
+      key: /\bUML\b/gi,
+      value: "UML",
+    }
+		,
+		{
+      key: /\bJVM\b/gi,
+      value: "JVM",
+    }
+		,
+		{
+      key: /\bNoSQL\b/gi,
+      value: "NoSQL",
+    }
+		,
+		{
+      key: /\bSSO\b/gi,
+      value: "SSO",
+    }
+		,
+		{
+      key: /\bCAS\b/gi,
+      value: "CAS",
+    }
+		,
+		{
+      key: /\bHTTP\b/gi,
+      value: "HTTP",
+    }
+		,
+		{
+      key: /\bHTTPS\b/gi,
+      value: "HTTPS",
+    }
+		,
+		{
+      key: /\bVS Code\b/gi,
+      value: "VS Code",
+    }
+		,
+		{
+      key: /\bJavaScript\b/gi,
+      value: "JavaScript",
+    }
+		,
+		{
+      key: /\bCSS\b/gi,
+      value: "CSS",
+    }
+		,
+		{
+      key: /\bJSON\b/gi,
+      value: "JSON",
+    }
+		,
+		{
+      key: /\bjQuery\b/gi,
+      value: "jQuery",
+    }
+		,
+		{
+      key: /\bAPI\b/gi,
+      value: "API",
+    }
+		,
+		{
+      key: /\bAJAX\b/gi,
+      value: "AJAX",
+    }
+		,
+		{
+      key: /\bHTML\b/gi,
+      value: "HTML",
+    }
+		,
+		{
+      key: /\bJSP\b/gi,
+      value: "JSP",
+    }
+		,
+		{
+      key: /\bFreeMarker\b/gi,
+      value: "FreeMarker",
+    }
+		,
+		{
+      key: /\bThymeleaf\b/gi,
+      value: "Thymeleaf",
+    }
+		,
+		{
+      key: /\biOS\b/gi,
+      value: "iOS",
+    }
+		,
+		{
+      key: /\bRocketMQ\b/gi,
+      value: "RocketMQ",
+    }
+		,
+		{
+      key: /\b\b/gi,
+      value: "",
+    }
+		,
+		{
+      key: /\bspringboot\b/gi,
+      value: "Spring Boot",
     }
 		
 ];
