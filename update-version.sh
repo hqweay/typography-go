51304b185c035bf41e45277620e668e09d741a57
hqweay <hqweay@gmail.com>
2024-04-04 00:29:16 +0800

    upgrade#v0.1.2

---
+++ update-version.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 从终端获取的参数值
+new_version="$1"
+
+jq --arg new_version "$new_version" '.version = $new_version' package.json > tmpfile && mv tmpfile package.json
+jq --arg new_version "$new_version" '.version = $new_version' package.json > tmpfile && mv tmpfile plugin.json
+
+git add package.json
+git add plugin.json
+git commit -m "upgrade#$new_version"
+git push
+git tag $new_version
+git push --tags
