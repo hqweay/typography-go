ec4b65b5b8e333c70718a8d34a3e8d7d2881eb79
hqweay <hqweay@gmail.com>
2024-04-04 18:00:20 +0800

    upgrade#0.1.7

--- update-version.sh
+++ update-version.sh
@@ -6,8 +6,9 @@ new_version="$1"
 jq --arg new_version "$new_version" '.version = $new_version' package.json > tmpfile && mv tmpfile package.json
 jq --arg new_version "$new_version" '.version = $new_version' plugin.json > tmpfile && mv tmpfile plugin.json
 
-git add package.json
-git add plugin.json
+# git add package.json
+# git add plugin.json
+git add .
 git commit -m "upgrade#$new_version"
 git push
 git tag $new_version
